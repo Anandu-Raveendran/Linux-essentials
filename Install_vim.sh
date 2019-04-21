be5250293927487a6e875c947142bb6f08cba2ce
Anandu-Raveendran <22anandu47@gmail.com>
2019-04-21 12:20:15 +0530

    install proper

--- Install_vim.sh
+++ Install_vim.sh
@@ -1,47 +1,50 @@
-echo "Installing vim"
+echo "\nAppending .vimrc to the current .vimrc file "
+cat .vimrc >> ~/.vimrc
+
+echo "\nInstalling vim"
 sudo apt-get install vim -y
-echo "Installing curl..."
+echo "\nInstalling curl..."
 sudo apt-get install curl -y
 
-echo "Installing ctags..."
+echo "\nInstalling ctags..."
 sudo apt-get install exuberant-ctags -y
 
-echo "Installing cscope..."
+echo "\nInstalling cscope..."
 sudo apt-get install cscope -y
 
-echo " Installing Pathogen"
+echo "\nInstalling Pathogen"
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-echo "Installing NerdTree"
+echo "\nInstalling NerdTree"
 cd ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
 
-echo "Installing tagbar"
+echo "\nInstalling tagbar"
 git clone git://github.com/majutsushi/tagbar
 
-echo "Installing sensible vim"
+echo "\nInstalling sensible vim"
 mkdir -p ~/.vim/pack/tpope/start
 cd ~/.vim/pack/tpope/start
 git clone https://tpope.io/vim/sensible.git
 
-echo "Installing supertab"
+echo "\nInstalling supertab"
 cd ~/.vim/bundle
 git clone https://github.com/ervandew/supertab
 
-echo "Installing ale"
+echo "\nInstalling ale"
 git clone https://github.com/w0rp/ale.git
 
-echo "Installing vim startify"
+echo "\nInstalling vim startify"
 git clone https://github.com/mhinz/vim-startify.git ~/.vim/bundle/vim-startify.vim
 
-echo "Installing vim commentary"
+echo "\nInstalling vim commentary"
 mkdir -p ~/.vim/pack/tpope/start
 cd ~/.vim/pack/tpope/start
 git clone https://tpope.io/vim/commentary.git
 
-echo "Installing vim highlight for cursor words"
+echo "\nInstalling vim highlight for cursor words"
 mkdir -p ~/.vim/bundle/vim-highlight-cursor-words
 git clone https://github.com/pboettch/vim-highlight-cursor-words.git ~/.vim/bundle/vim-highlight-cursor-words
 
-echo "Done. Now copy .vimrc file to /home/user/"
+echo "\nDone." 
